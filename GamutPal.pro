4425dd201d079ca5f5271c430e1348acf606b612
Darryl T. Agostinelli <dagostinelli@gmail.com>
2016-08-23 12:28:31 -0500

    save

---
+++ GamutPal.pro
@@ -0,0 +1,26 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-07-16T19:15:17
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = GamutPal
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+DISTFILES += \
+    YWheel_Course.png
+
+RESOURCES += \
+    resources.qrc
---
+++ mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QtDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+	ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+	delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+	::exit(0);
+}
+
+void MainWindow::on_backgroundSlider_sliderMoved(int position)
+{
+	int rgb = ((double)position / 100.0) * 255.0;
+	QPalette Pal(palette());
+	Pal.setColor(QPalette::Background, QColor::fromRgb(rgb,rgb,rgb));
+	this->ui->widget->setAutoFillBackground(true);
+	this->ui->widget->setPalette(Pal);
+}

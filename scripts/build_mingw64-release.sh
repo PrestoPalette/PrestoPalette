cee986f595e0e58e2a3e19c2d5c1b6d34f2b0429
Darryl T. Agostinelli <dagostinelli@gmail.com>
2017-03-13 07:51:56 -0500

    added nsis installer back

--- scripts/build_mingw64-release.sh
+++ scripts/build_mingw64-release.sh
@@ -8,7 +8,6 @@ mkdir build
 cd build
 
 mingw64-cmake -G "Unix Makefiles" .. -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-mingw64.cmake
-make && ctest && make install && cpack -G "TGZ"
-#make && ctest && make install && cpack -G "TGZ" && cpack -G "NSIS64"
+make && ctest && make install && cpack -G "TGZ" && cpack -G "NSIS64"
 
 popd

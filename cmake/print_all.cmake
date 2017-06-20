862a1a56ef549ff903dffa55ba7b4e5a607dc7cd
Darryl T. Agostinelli <dagostinelli@users.noreply.github.com>
2017-06-20 10:14:25 -0500

    Delete print_all.cmake

--- cmake/print_all.cmake
+++
@@ -1,6 +0,0 @@
-function(print_all_properties)
-	get_cmake_property(_variableNames VARIABLES)
-	foreach (_variableName ${_variableNames})
-		message(STATUS "${_variableName}=${${_variableName}}")
-	endforeach()
-endfunction()

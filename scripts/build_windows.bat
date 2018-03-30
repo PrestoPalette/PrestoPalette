1e4f81b4ed41fa2bffe8373d7da38617ffaf6903
Darryl T. Agostinelli <darryl.agostinelli@saggiotechnologies.com>
2018-03-30 18:00:59 -0500

    updated windows build script

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -5,7 +5,8 @@ PUSHD "%~dp0"
 cd ..
 
 qmake.exe -config debug_and_release PrestoPalette.pro
-nmake
+nmake all
+windeployqt.exe build\debug\PrestoPalette.exe
 windeployqt.exe build\release\PrestoPalette.exe
 iscc PrestoPalette.iss "/DCustomBinaryLocation=build\release"
 iscc PrestoPalette.iss "/DCustomBinaryLocation=build\debug" "/DConfiguration=Debug"
